c4a86abb658914d8679fdbd24913da455574e53e
craigweighman <craigweighman@gmail.com>
2019-04-18 14:55:30 +0100

    quote data updated

--- src/quote-data.js
+++ src/quote-data.js
@@ -3,13 +3,13 @@ export const quotes = [
     0: "less, but better"
   },
   {
-    1: "Good design is innovative, Good design makes a product useful, Good design is aesthetic"
+    1: "Good design is innovative"
   },
   {
-    2: "Good design makes a product understandable, Good design is thorough down to the last detail"
+    2: "Good design makes a product understandable"
   },
   {
-    3: "Good design is unobtrusive, Good design is honest, Good design is long lasting"
+    3: "Good design is unobtrusive"
   },
   {
     4: "Good design is as little design as possible"
@@ -18,7 +18,7 @@ export const quotes = [
     5: "Having small touches of colour makes it more colourful than having the whole thing in colour"
   },
   {
-    6: "Indifference towards people and the reality in which they live is actually the one and only cardinal sin in design"
+    6: "Indifference towards people and the reality in which they live is the one and only cardinal sin in design"
   },
   {
     7: "Back to purity, back to simplicity"
@@ -30,7 +30,7 @@ export const quotes = [
     9: "You cannot understand good design if you do not understand people"
   },
   {
-    10: "Good design is making something intelligible and memorable - Great design is making something memorable and Meaningful"
+    10: "Great design is making something memorable and Meaningful"
   },
   {
     11: "My heart belongs to the details, I always found them to be more important than the big picture"
@@ -39,7 +39,7 @@ export const quotes = [
     12: "Nothing works without details, They are everything - the baseline of quality"
   },
   {
-    13: "One of the most significant design principles is to omit the unimportant in order to emphasize the important"
+    13: "Omit the unimportant in order to emphasize the important"
   },
   {
     14: "Design should not dominate people - it should help people, That's its role"
@@ -49,5 +49,47 @@ export const quotes = [
   },
   {
     16: "Good design is environmentally friendly"
+  },
+  {
+    17: "Good design makes a product useful"
+  },
+  {
+    18: "Good design is aesthetic"
+  },
+  {
+    19: "Good design is thorough down to the last detail"
+  },
+  {
+    20: "Good design is honest"
+  },
+  {
+    21: "Good design is long lasting"
+  },
+  {
+    22: "I believe designers should eliminate the unnecessary"
+  },
+  {
+    23: "I sit, i think, i make some drawings"
+  },
+  {
+    24: "the unspectacular things are the important things"
+  },
+  {
+    25: "as designers, we have a great responsibility"
+  },
+  {
+    26: "as a designer, you cannot retire totally"
+  },
+  {
+    27: "quiet is better than loud"
+  },
+  {
+    28: "simplicity, essentiality, and openness"
+  },
+  {
+    29: "only well-executed objects can be beautiful"
+  },
+  {
+    30: "weniger, aber besser"
   }
 ];
