ed1310452ea93c306008394181b2fb8e32fa256f
craigweighman <45236597+craigweighman@users.noreply.github.com>
2021-06-08 07:07:59 +0100

    Update Rams.js

--- src/component/Rams.js
+++ src/component/Rams.js
@@ -4,7 +4,7 @@ import "../style/Rams.css";
 const Rams = ({ quoteId, click }) => {
   return <div className="rams" onClick={click} 
     style={{
-         background: quoteId < 8 ? url(../rams.jpg) : 
+         background: quoteId < 8 ? url(../rams-one.jpg) : 
                       quoteId < 16 ? url(../rams-one.jpg) :
                       quoteId < 24 ? url(../rams-two.jpg) :
                       url(../rams-three.jpg)
