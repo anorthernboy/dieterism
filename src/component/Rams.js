267f3b138214d74c2bc23989409f91611f51333c
craigweighman <45236597+craigweighman@users.noreply.github.com>
2021-06-08 06:38:30 +0100

    Update Rams.js

--- src/component/Rams.js
+++ src/component/Rams.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../style/Rams.css";
 
 const Rams = ({ click }) => {
-  return <div className="rams" onClick={click} />;
+  return <div className="rams" onClick={click} style="url(../rams.jpg)"/>;
 };
 
 export default Rams;
