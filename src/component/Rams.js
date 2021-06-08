881f3260441f1e6c7aecffe73e0f53875b4c9f12
craigweighman <45236597+craigweighman@users.noreply.github.com>
2021-06-08 07:05:51 +0100

    Update Rams.js

--- src/component/Rams.js
+++ src/component/Rams.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "../style/Rams.css";
 
-const Rams = ({ click }) => {
-  return <div className="rams" onClick={click} style="url(../rams.jpg)"/>;
+const Rams = ({ quoteId, click }) => {
+  return <div className="rams" onClick={click} 
+    style={{
+         background: quoteId < 8 ? url(../rams.jpg) : 
+                      quoteId < 16 ? url(../rams-one.jpg) :
+                      quoteId < 24 ? url(../rams-two.jpg) :
+                      url(../rams-three.jpg)
+        }}
+  />;
 };
 
 export default Rams;
