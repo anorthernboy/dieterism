0cdd327edfa0f967ea2b4ca002d6756114929cee
craigweighman <45236597+craigweighman@users.noreply.github.com>
2021-06-08 07:54:49 +0100

    Update Rams.js

--- src/component/Rams.js
+++ src/component/Rams.js
@@ -8,13 +8,10 @@ import RamsImgThree from "../rams-three.jpg";
 const Rams = ({ quoteId, click }) => {
   return <div className="rams" onClick={click} 
     style={{
-         background: quoteId < 8 ? `url(${RamsImg})` :
-                      quoteId < 16 ? `url(${RamsImgOne})` :
-                      quoteId < 24 ? `url(${RamsImgTwo})` :
-                      `url(${RamsImgThree})`,
-         backgroundSize: 'contain',
-         backgroundPosition: 'center',
-         backgroundRepeat: 'no-repeat',
+         background: quoteId < 8 ? `url(${RamsImg}) center center / contain no-repeat` :
+                      quoteId < 16 ? `url(${RamsImgOne}) center center / contain no-repeat` :
+                      quoteId < 24 ? `url(${RamsImgTwo}) center center / contain no-repeat` :
+                      `url(${RamsImgThree}) center center / contain no-repeat`,
          height: '300px',
          width: '320px',
          animation: 'pulse 1.5s infinite'
